08853cca2edd131cff4b4d3517e00ad32b835f40
BW Personal <benjamin.weber@flagship-apps.de>
2024-08-09 09:09:07 +0200

    dont rely on test timing

--- test/queue_test.dart
+++ test/queue_test.dart
@@ -218,50 +218,60 @@ void main() {
     final results = <String?>[];
     final errors = <Exception>[];
 
+    final completer1 = Completer();
+    final completer2 = Completer();
+    final completer3 = Completer();
+    final completer4 = Completer();
+    final completer5 = Completer();
+
     unawaited(Future.wait([
       cancelQueue
           .add(() async {
-            await Future.delayed(const Duration(milliseconds: 10));
+            await completer1.future;
             return "result 1";
           })
           .then((result) => results.add(result))
           .catchError((err) => errors.add(err)),
       cancelQueue
           .add(() async {
-            await Future.delayed(const Duration(milliseconds: 10));
+            await completer2.future;
             return "result 2";
           })
           .then((result) => results.add(result))
           .catchError((err) => errors.add(err)),
       cancelQueue
           .add(() async {
-            await Future.delayed(const Duration(milliseconds: 10));
+            await completer3.future;
             return "result 3";
           })
           .then((result) => results.add(result))
           .catchError((err) => errors.add(err)),
       cancelQueue
           .add(() async {
-            await Future.delayed(const Duration(milliseconds: 10));
+            await completer4.future;
             return "result 4";
           })
           .then((result) => results.add(result))
           .catchError((err) => errors.add(err)),
       cancelQueue
           .add(() async {
-            await Future.delayed(const Duration(milliseconds: 10));
+            await completer5.future;
             return "result 5";
           })
           .then((result) => results.add(result))
           .catchError((err) => errors.add(err))
     ]));
 
-    await Future.delayed(const Duration(milliseconds: 25));
+    completer1.complete();
+    completer2.complete();
+    await Future.delayed(Duration.zero);
     cancelQueue.cancel();
+    completer3.complete();
     await cancelQueue.onComplete;
     expect(results.length, 3);
     expect(errors.length, 2);
-    expect(errors.first is QueueCancelledException, true);
+
+    expect(errors.first, isA<QueueCancelledException>());
   });
 
   test("timed out queue item still completes", () async {
