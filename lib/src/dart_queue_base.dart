2734fa682f444c58c1fde6fd813fa7abcf1300cd
Ryan Knell <ryan.knell@gmail.com>
2025-01-01 22:49:34 +1000

    Update add method with addToFront parameter

--- lib/src/dart_queue_base.dart
+++ lib/src/dart_queue_base.dart
@@ -118,13 +118,16 @@ class Queue {
   /// Adds the future-returning closure to the queue.
   ///
   /// It will be executed after futures returned
-  /// by preceding closures have been awaited.
+  /// by preceding closures have been awaited if [addToFront] is false. If
+  /// [addToFront] is true it gets executed within the next free slot by being
+  /// added to the front of the queue.
   ///
   /// Will throw an exception if the queue has been cancelled.
-  Future<T> add<T>(Future<T> Function() closure) {
+  Future<T> add<T>(Future<T> Function() closure, {bool addToFront = false}) {
     if (isCancelled) throw QueueCancelledException();
     final completer = Completer<T>();
-    _nextCycle.add(_QueuedFuture<T>(closure, completer, timeout));
+    final item = _QueuedFuture<T>(closure, completer, timeout);
+    addToFront ? _nextCycle.insert(0, item) : _nextCycle.add(item);
     _updateRemainingItems();
     unawaited(_process());
     return completer.future;
